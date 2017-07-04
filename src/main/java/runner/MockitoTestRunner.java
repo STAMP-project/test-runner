36646cb6f7c2e18fc0b10a9058663bf94185f4e9
danglotb <bdanglot@gmail.com>
2017-07-04 16:34:09 +0200

    add mockito runner

--- src/main/java/runner/MockitoTestRunner.java
+++ src/main/java/runner/MockitoTestRunner.java
@@ -1,11 +1,17 @@
 package runner;
 
+import filter.MethodFilter;
 import listener.TestListener;
+import org.junit.runner.Description;
 import org.junit.runner.Request;
 import org.junit.runner.Runner;
+import org.junit.runner.manipulation.Filter;
 import org.junit.runner.notification.RunNotifier;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -24,35 +30,48 @@ public class MockitoTestRunner extends  AbstractTestRunner {
     }
 
     @Override
-    public TestListener run(Map<String, Collection<String>> testMethodNamesForClasses) {
-        return null;
-    }
-
-    @Override
-    public TestListener run(Collection<String> fullQualifiedNames) {
-        return null;
-    }
-
-    @Override
     public TestListener run(String fullQualifiedName, Collection<String> testMethodNames) {
-        return null;
+        try {
+            TestListener listener = new TestListener();
+            MockitoJUnitRunner runner = new MockitoJUnitRunner(this.loadClass(fullQualifiedName));
+            runner.filter(new MethodFilter(testMethodNames));
+            RunNotifier runNotifier = new RunNotifier();
+            runNotifier.addFirstListener(listener);
+            runner.run(runNotifier);
+            return listener;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     @Override
     public TestListener run(String fullQualifiedName, String testMethodName) {
-        return null;
+        try {
+            TestListener listener = new TestListener();
+            MockitoJUnitRunner runner = new MockitoJUnitRunner(this.loadClass(fullQualifiedName));
+            runner.filter(new MethodFilter(Collections.singletonList(testMethodName)));
+            RunNotifier runNotifier = new RunNotifier();
+            runNotifier.addFirstListener(listener);
+            runner.run(runNotifier);
+            return listener;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public TestListener run(String fullQualifiedName) {
-        TestListener listener = new TestListener();
-        Request request = Request.classes(this.loadClass(fullQualifiedName));
-
-        Runner runner = request.getRunner();
-        RunNotifier runNotifier = new RunNotifier();
-        runNotifier.addFirstListener(listener);
-        runner.run(runNotifier);
-        return listener;
+        try {
+            TestListener listener = new TestListener();
+            MockitoJUnitRunner runner = new MockitoJUnitRunner(this.loadClass(fullQualifiedName));
+            RunNotifier runNotifier = new RunNotifier();
+            runNotifier.addFirstListener(listener);
+            runner.run(runNotifier);
+            return listener;
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
