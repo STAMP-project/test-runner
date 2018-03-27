7504180670bef7798d22a747cb407dfbc9426605
danglotb <bdanglot@gmail.com>
2018-03-27 14:24:15 +0200

    doc: improve documentation on runners, that should not be used direclty

--- src/main/java/eu/stamp/project/testrunner/runner/test/TestRunner.java
+++ src/main/java/eu/stamp/project/testrunner/runner/test/TestRunner.java
@@ -18,7 +18,9 @@ public class TestRunner {
     /**
      * The entry method to execute junit tests.
      * This method is not meant to be used directly, but rather using {@link eu.stamp.project.testrunner.EntryPoint}
-     * @param args this array should be build by {@link eu.stamp.project.testrunner.EntryPoint}
+     * @param args this array should be build by {@link eu.stamp.project.testrunner.EntryPoint}.
+     *             the first argument is the full qualified name of the test class
+     *             the second argument is optionally the list of the test method name separated by ":".
      * @throws ClassNotFoundException in case of the supplied classpath is wrong
      */
     public static void main(String[] args) throws ClassNotFoundException {
