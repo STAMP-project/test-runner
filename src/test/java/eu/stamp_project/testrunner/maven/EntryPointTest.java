c86149cc228f1ddd24957586d95a78875f55963a
danglotb <benjamin.danglot@inria.fr>
2018-09-12 15:38:17 +0200

    test: add a test using the maven feature of the test runner

--- src/test/java/eu/stamp_project/testrunner/maven/EntryPointTest.java
+++ src/test/java/eu/stamp_project/testrunner/maven/EntryPointTest.java
@@ -1,6 +1,13 @@
 package eu.stamp_project.testrunner.maven;
 
+import eu.stamp_project.testrunner.runner.test.Failure;
+import eu.stamp_project.testrunner.runner.test.TestListener;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by Benjamin DANGLOT
@@ -9,23 +16,54 @@ import org.junit.Test;
  */
 public class EntryPointTest {
 
+    @Before
+    public void setUp() throws Exception {
+        EntryPoint.runMavenGoal("src/test/resources/test-projects", "clean");
+    }
 
     @Test
-    public void test() {
+    public void testOnFailing() {
 
         /*
-            Test the EntryPoint using maven
+            Test the EntryPoint using maven on failing test
+
+                When running with maven, assumption failure are consider as ignored.
          */
 
-        EntryPoint.runTestClasses("/home/bdanglot/workspace/testrunner/src/test/resources/test-projects/pom.xml");
-        EntryPoint.runTestClasses("/home/bdanglot/workspace/testrunner/src/test/resources/test-projects/pom.xml",
-                "example.TestSuiteExample", "example.TestSuiteExample"
-        );
-        EntryPoint.runTests("/home/bdanglot/workspace/testrunner/src/test/resources/test-projects/pom.xml",
-                "example.TestSuiteExample", "test2", "test3", "test7"
-        );
+        final TestListener testListener =  EntryPoint.runTestClasses("src/test/resources/test-projects", "failing.FailingTestClass");
+        assertEquals(2, testListener.getRunningTests().size());
+        assertEquals(1, testListener.getPassingTests().size());
+        assertEquals(1, testListener.getFailingTests().size());
+        assertEquals(0, testListener.getAssumptionFailingTests().size());
+        assertEquals(2, testListener.getIgnoredTests().size());
 
-        
+        final Failure testFailing = testListener.getFailureOf("testFailing");
+        assertEquals("testFailing", testFailing.testCaseName);
 
+        try {
+            testListener.getFailureOf("testPassing");
+            fail("Should have throw IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true); // expected
+        }
     }
+
+    @Test
+    public void testOnSpecificTestMethod() {
+
+        /*
+            Test the EntryPoint using maven on failing test
+
+                When running with maven, assumption failure are consider as ignored.
+         */
+
+        final TestListener testListener =  EntryPoint.runTests("src/test/resources/test-projects",
+                "example.TestSuiteExample", "test2");
+        assertEquals(1, testListener.getRunningTests().size());
+        assertEquals(1, testListener.getPassingTests().size());
+        assertEquals(0, testListener.getFailingTests().size());
+        assertEquals(0, testListener.getAssumptionFailingTests().size());
+        assertEquals(0, testListener.getIgnoredTests().size());
+    }
+
 }
