b56461222a0a4ac6fe18942087d1cdc173b7148a
danglotb <benjamin.danglot@inria.fr>
2018-11-28 11:45:06 +0100

    resources: updates binaries to be run

--- src/update-classes-in-resources.sh
+++ src/update-classes-in-resources.sh
@@ -5,12 +5,14 @@ mvn clean compile
 rm -rf src/main/resources/runner-classes/
 
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/
+mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/utils/
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/impl
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/junit4
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/junit5
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/runner/
 mkdir --parent src/main/resources/runner-classes/eu/stamp_project/testrunner/runner/coverage/
 
+cp -r target/classes/eu/stamp_project/testrunner/utils/*.class src/main/resources/runner-classes/eu/stamp_project/testrunner/utils/
 cp -r target/classes/eu/stamp_project/testrunner/listener/*.class src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/
 cp -r target/classes/eu/stamp_project/testrunner/listener/impl/*.class src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/impl/
 cp -r target/classes/eu/stamp_project/testrunner/listener/junit4/*.class src/main/resources/runner-classes/eu/stamp_project/testrunner/listener/junit4/
