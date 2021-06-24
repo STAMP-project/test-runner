40ccab31b60a333ab15be130ed3e59f0e11c45f9
André Silva <andre15andre@hotmail.com>
2021-06-24 14:25:29 +0200

    Update tests.
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- src/test/java/eu/stamp_project/testrunner/runner/coverage/JUnit5JacocoRunnerCoveredResultPerTestMethodTest.java
+++ src/test/java/eu/stamp_project/testrunner/runner/coverage/JUnit5JacocoRunnerCoveredResultPerTestMethodTest.java
@@ -10,40 +10,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class JUnit4JacocoRunnerCoveredResultPerTestMethodTest extends AbstractTest {
+public class JUnit5JacocoRunnerCoveredResultPerTestMethodTest extends AbstractTest {
 
-	// FIXME: The behaviour in this case is not consistent
-	// There should be an equal number of keys in both running tests and the coverage map
-	// There should also always be a key for the method given as input
-	@Test
+	// FIXME: Parametrized tests are not yet supported for JUnit5
 	@Ignore
+	@Test
 	public void testWithoutNewJvmOnTestClassParametrized() throws Exception {
 
         /*
             Using the api to compute the coverage on a test class
          */
 
-		JUnit4JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
+		JUnit5JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
 						ParserOptions.FLAG_pathToCompiledClassesOfTheProject, TEST_PROJECT_CLASSES,
-						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "example.ParametrizedTest",
+						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "junit5.ParametrizedTest",
 						ParserOptions.FLAG_testMethodNamesToRun, "test"
 				}
 		);
 		final CoveredTestResultPerTestMethod load = CoveredTestResultPerTestMethodImpl.load();
 		System.out.println(load);
-
-		assertEquals(5, load.getRunningTests().size());
-		assertEquals(5, load.getPassingTests().size());
-		assertEquals(0, load.getFailingTests().size());
-		assertEquals(0, load.getIgnoredTests().size());
-
-		assertEquals(5, load.getCoverageResultsMap().size());
 		assertEquals(34, load.getCoverageResultsMap().get("test").getInstructionsCovered());
-		assertEquals(23, load.getCoverageResultsMap().get("test[0]").getInstructionsCovered());
-		assertEquals(26, load.getCoverageResultsMap().get("test[1]").getInstructionsCovered());
-		assertEquals(19, load.getCoverageResultsMap().get("test[2]").getInstructionsCovered());
-		assertEquals(19, load.getCoverageResultsMap().get("test[3]").getInstructionsCovered());
-
 		System.out.println(load.getCoverageResultsMap().get("test").getExecutionPath());
 	}
 
@@ -54,26 +40,24 @@ public class JUnit4JacocoRunnerCoveredResultPerTestMethodTest extends AbstractTe
             Using the api to compute the coverage on test cases
          */
 
-		JUnit4JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
+		JUnit5JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
 						ParserOptions.FLAG_pathToCompiledClassesOfTheProject, TEST_PROJECT_CLASSES,
-						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "example.TestSuiteExample",
-						ParserOptions.FLAG_testMethodNamesToRun, "test3:test2:copyOftest2"
+						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "junit5.TestSuiteExample",
+						ParserOptions.FLAG_testMethodNamesToRun, "test3:test2"
 				}
 		);
 		final CoveredTestResultPerTestMethod load = CoveredTestResultPerTestMethodImpl.load();
 		System.out.println(load);
 
-		assertEquals(3, load.getRunningTests().size());
-		assertEquals(3, load.getPassingTests().size());
+		assertEquals(2, load.getRunningTests().size());
+		assertEquals(2, load.getPassingTests().size());
 		assertEquals(0, load.getFailingTests().size());
 		assertEquals(0, load.getIgnoredTests().size());
 
-		assertEquals(3, load.getCoverageResultsMap().size());
+		assertEquals(2, load.getCoverageResultsMap().size());
 		assertEquals(23, load.getCoverageResultsMap().get("test2").getInstructionsCovered());
 		assertEquals(23, load.getCoverageResultsMap().get("test3").getInstructionsCovered());
-		assertEquals(23, load.getCoverageResultsMap().get("copyOftest2").getInstructionsCovered());
 		System.out.println(load.getCoverageResultsMap().get("test2").getExecutionPath());
-		System.out.println(load.getCoverageResultsMap().get("copyOftest2").getExecutionPath());
 		System.out.println(load.getCoverageResultsMap().get("test3").getExecutionPath());
 	}
 
@@ -84,13 +68,12 @@ public class JUnit4JacocoRunnerCoveredResultPerTestMethodTest extends AbstractTe
             Using the api to compute the coverage on test cases
          */
 
-		JUnit4JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
+		JUnit5JacocoRunnerCoveredResultPerTestMethod.main(new String[]{
 						ParserOptions.FLAG_pathToCompiledClassesOfTheProject, TEST_PROJECT_CLASSES,
-						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "example.TestSuiteExample",
+						ParserOptions.FLAG_fullQualifiedNameOfTestClassToRun, "junit5.TestSuiteExample",
 				}
 		);
 		final CoveredTestResultPerTestMethod load = CoveredTestResultPerTestMethodImpl.load();
-
 		System.out.println(load);
 		load.getCoverageResultsMap()
 				.keySet()
