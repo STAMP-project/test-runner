07e6412ffbd7fa2dc869eb4c22d8bd7ea557b015
danglotb <benjamin.danglot@davidson.fr>
2022-02-01 09:57:40 +0100

    ci: enable new workflows to increase the version and deploy a stable version on maven central

---
+++ .github/buildscript/deploy_to_maven.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Deploy a jar, source jar, and javadoc jar to Sonatype Maven Central repo.
+#
+# Inspired from https://github.com/square/retrofit/blob/fccedbeb4d5181c926fff450cdf5c5116ef0eeaa/.buildscript/deploy_snapshot.sh
+
+set -e
+
+echo "Deploying ..."
+# made with symmetric key encryption algorithm AES256
+# reference: https://docs.github.com/en/actions/security-guides/encrypted-secrets#limits-for-secrets
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" --output test-runner.gpg .buildscript/test-runner.gpg.enc
+gpg --fast-import test-runner.gpg
+
+# getting the previous version on Maven Central
+# for some reasons, some versions don't get index by search.maven.org/solrsearch/, and it break the build
+# so we have to roll our own: https://gist.github.com/monperrus/9ee373e2500e40b634f8daf707f6ad2a
+# PREVIOUS_MAVEN_CENTRAL_VERSION=`curl "http://search.maven.org/solrsearch/select?q=a:gumtree-spoon-ast-diff+g:fr.inria.gforge.spoon.labs&rows=20&wt=json" | jq -r .response.docs[0].latestVersion | egrep -o "[0-9]+$"`
+PREVIOUS_MAVEN_CENTRAL_VERSION=`curl "https://www.monperrus.net/martin/last-version-maven.py?groupId=eu.stamp-project&artifactId=test-runner" | egrep -o "[0-9]+$"`
+
+# and incrementing it
+mvn versions:set -DnewVersion=1.$((PREVIOUS_MAVEN_CENTRAL_VERSION+1))
+
+mvn -Prelease deploy --settings .buildscript/settings.xml -Dmaven.test.skip=true
+echo "Well deployed!"
